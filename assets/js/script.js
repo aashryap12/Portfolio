7558ac4642d9459a407a09936051a710cc8d9eba
aashryap12 <165940417+aashryap12@users.noreply.github.com>
2024-07-28 18:04:30 +0530

    projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,6 +98,7 @@ function showSkills(skills) {
 function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
+    /*
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
         <div class="box tilt">
